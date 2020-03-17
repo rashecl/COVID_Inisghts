b062d40130549798b9f0156f5d61787bca162ada
rashecl <31122647+rashecl@users.noreply.github.com>
2020-03-17 15:56:44 -0700

    beta code for tracking coronavirus in the US

---
+++ COVID/us_map.py
@@ -0,0 +1,19 @@
+from bokeh.sampledata.us_states import data as stateBorders
+from bokeh.sampledata.us_counties import data as counties
+import pandas as pd
+
+# stateBorders['D.C.'] = stateBorders.pop('DC')
+stateBorders= pd.DataFrame(stateBorders)  # This contains boundaries for each state
+# excludeStates = ['AK', 'HI'] # Exclude the states
+stateBorders = stateBorders.drop(columns=['AK', 'HI'])
+
+countyBorders = pd.DataFrame(columns = ['county_name', 'state', 'lats', 'lons'])
+for code, county in counties.items():
+    county_name = county['detailed name'].split(', ')[0]
+    state=county['state'].upper()
+    lats=county['lats']
+    lons=county['lons']
+    countyBorders = countyBorders.append({'county_name': county_name,
+                    'state': state,
+                    'lats': lats,
+                    'lons': lons}, ignore_index=True)
--- US_coronavirus_map.py
+++ US_coronavirus_map.py
@@ -1,80 +1,142 @@
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
 from bokeh.io import show
 from bokeh.layouts import column, row
 from bokeh.io import curdoc
-from bokeh.models import LogColorMapper, LinearColorMapper, HoverTool
+from bokeh.models import LogColorMapper, LinearColorMapper, HoverTool, ColumnDataSource, CheckboxGroup
 from bokeh.palettes import RdYlBu11 as palette
 from bokeh.plotting import figure
-from bokeh.sampledata.us_states import data as states
-from bokeh.sampledata.us_counties import data as counties
 from COVID.extract import JHU
+from COVID import us_map
 from random import random as rand
-import numpy as np
 import pandas as pd
 
-states = pd.DataFrame(states)  # This contains boundaries for each state
-states['D.C.'] = states.pop('DC')
+DataSelectButtons = CheckboxGroup(labels=["Cases of Coronavirus", "Restaurant Dining", "Employment rates"], active=[0])
+
+stateBorders = us_map.stateBorders
+countyBorders = us_map.countyBorders
+
 statesDF = JHU.getStateDF()
-countries = JHU.getCountryDF()
+state_regionalDF = JHU.getState_RegionalDF()
+countriesDF = JHU.getCountriesDF()
+countyDF = JHU.getCountiesDF()
 
 palette = tuple(palette)
 
-excludeStates = ['AK', 'HI', 'PR', 'VI']
-state_xs = [states[state]["lons"] for state in states if state if state not in excludeStates]
-state_ys = [states[state]["lats"] for state in states if state if state not in excludeStates]
+state_xs = [stateBorders[state]["lons"] for state in stateBorders if state]
+state_ys = [stateBorders[state]["lats"] for state in stateBorders if state]
 
-state_names = [states[state]["name"] for state in states if state not in excludeStates]
+state_names = [stateBorders[state]["name"] for state in stateBorders]
 # state_names = [county['state'] for county in counties.values()]
 
 state_val = []
-for state in states:
-    if (state in statesDF.columns) and (state not in excludeStates):
-        state_val.append(statesDF[state].confirmed[-1])
-    elif state not in excludeStates:
-        print(state + ' does not have any cases')
+for state in stateBorders:
+    if (state in statesDF.columns):
+        state_val.append(np.max(statesDF[state].confirmed))
+    else:
+        print(state + ' does not have any records of cases')
         state_val.append(0)
 
 color_mapper = LinearColorMapper(palette=palette)
 
-usData = dict(
+usData = ColumnDataSource(data = dict(
     x=state_xs,
     y=state_ys,
     val=state_val,
-    state=state_names)
+    state=state_names))
 
-gData = dict(
-    date=countries['US'].time.astype(str),
-    time=countries['US'].time,
-    confirmed=countries['US'].confirmed,
-    recovered=countries['US'].recovered,
-    deaths=countries['US'].deaths)
+gData = ColumnDataSource(data=dict(
+    date=countriesDF['US'].time.astype(str),
+    time=countriesDF['US'].time,
+    confirmed=countriesDF['US'].confirmed,
+    recovered=countriesDF['US'].recovered,
+    deaths=countriesDF['US'].deaths))
 
-state_counties = {
-    code: county for code, county in counties.items() if county["state"] == 'ca'}
+TOOLS = "pan,wheel_zoom, tap, reset,hover,save"
+TOOLS2 = "pan,wheel_zoom,reset,save"
 
-county_xs = [county["lons"] for county in state_counties.values()]
-county_ys = [county["lats"] for county in state_counties.values()]
+graphPlot = figure(tools=TOOLS2, x_axis_type='datetime', tooltips=[
+               ("Date", "@date"), ("confirmed", "@confirmed"), ('recovered', '@recovered'), ('died', '@deaths')
+           ], width=300, height=300)
+graphPlot.line('time', 'confirmed', source=gData, line_color='black')
+graphPlot.line('time', 'recovered', source=gData, line_color='green')
+graphPlot.line('time', 'deaths', source=gData, line_color='red')
+graphPlot.yaxis.axis_label = '# of people'
+graphPlot.xaxis.axis_label = 'Date'
 
-county_names = [county['name'] for county in state_counties.values()]
-state_names = [county['state'] for county in state_counties.values()]
+stateData = ColumnDataSource(data={'x': [], 'y': [], 'name': [], 'val': [], 'state': []})
 
-county_val = [rand() for county_id in state_counties]
 color_mapper = LinearColorMapper(palette=palette)
+color_mapper2 = LogColorMapper(palette=palette)
 
-stateData=dict(
-    x=county_xs,
-    y=county_ys,
-    name=county_names,
-    val=county_val,
-    state=state_names
-)
-
-
-TOOLS = "pan,wheel_zoom, reset,hover,save"
-
-usPlot = figure(title="Coronavirus map", tools=TOOLS,
+statePlot = figure(title="Coronavirus map", tools=TOOLS,
                 x_axis_location=None, y_axis_location=None,
-                tooltips=[("value", "@val"), ("(Long, Lat)", "($x, $y)"), ('State', '@state')
-           ], width=60 * 15, height=27 * 15)
+                tooltips=[('name','@name'),("value", "@val"), ("(Long, Lat)", "($x, $y)"), ('State', '@state')],
+                   height=300)
+
+statePlot.grid.grid_line_color = None
+statePlot.hover.point_policy = "follow_mouse"
+statePlot.patches('x', 'y', source=stateData,
+               fill_color={'field': 'val', 'transform': color_mapper2},
+               fill_alpha=0.7, line_color="white", line_width=0.5)
+
+state = 'CA'
+stateCountyDF = pd.DataFrame()
+
+def updateState():
+    global stateCountyDF, state
+    print(state)
+    stateCountyDF = state_regionalDF[state_regionalDF['state'] == state]
+    stateCountyBorders = countyBorders[countyBorders['state'] == state]
+    county_xs = [stateCountyBorders.iloc[i, :]['lons'] for i in range(len(stateCountyBorders))]
+    county_ys = [stateCountyBorders.iloc[i, :]['lats'] for i in range(len(stateCountyBorders))]
+    county_names = [stateCountyBorders.iloc[i, :]['county_name'] for i in range(len(stateCountyBorders))]
+    state_names = [state for i in range(len(stateCountyBorders))]
+    # county_val = [rand() for i in range(len(stateCounties))]
+    county_val = []
+    for county in county_names:
+        if county in list(stateCountyDF['county_name']):
+            confirmed_t = stateCountyDF[stateCountyDF['county_name'] == county].confirmed.values[0]
+            if np.isnan(confirmed_t[-1]):
+                lastIdx = len(confirmed_t) - 2
+            else:
+                lastIdx = len(confirmed_t) - 1
+            county_val.append(np.max(confirmed_t[:lastIdx]))
+            print(np.max(confirmed_t[:lastIdx]))
+        else:
+            print(county)
+            county_val.append(0)
+
+    stateData.data = dict(
+        x=county_xs,
+        y=county_ys,
+        name=county_names,
+        val=county_val,
+        state=state_names)
+
+    gData.data = dict(
+        date=statesDF[state].time.astype(str),
+        time=statesDF[state].time,
+        confirmed=statesDF[state].confirmed,
+        recovered=statesDF[state].recovered,
+        deaths=statesDF[state].deaths)
+
+    # statePlot.x_range.start = -125
+    # statePlot.x_range.end = -65
+    # statePlot.y_range.start = 23
+    # statePlot.y_range.end = 50
+
+    graphPlot.title.text = stateBorders[state]['name']
+    statePlot.title.text = stateBorders[state]['name']
+    return
+
+updateState()
+
+usPlot = figure(title="Cases of Coronavirus", tools=TOOLS,
+                x_axis_location=None, y_axis_location=None,
+                tooltips=[("value", "@val"), ("(Long, Lat)", "($x, $y)"), ('State', '@state')],
+                width=60 * 15, height=27 * 15)
 usPlot.grid.grid_line_color = None
 usPlot.x_range.start = -125
 usPlot.x_range.end = -65
@@ -82,38 +144,75 @@ usPlot.y_range.start = 23
 usPlot.y_range.end = 50
 usPlot.hover.point_policy = "follow_mouse"
 
-TOOLS2 = "pan,wheel_zoom,reset,save"
-graphPlot = figure(tools=TOOLS2, x_axis_type='datetime', tooltips=[
-               ("Date", "@date"), ("confirmed", "@confirmed"), ('recovered', '@recovered'), ('died', '@deaths')
-           ], width=300, height=300)
-graphPlot.line('time', 'confirmed', source=gData, line_color='black')
-graphPlot.line('time', 'recovered', source=gData, line_color='green')
-graphPlot.line('time', 'deaths', source=gData, line_color ='red')
-graphPlot.yaxis.axis_label = '# of people'
-graphPlot.xaxis.axis_label = 'Date'
-
 usPlot.patches('x', 'y', source=usData,
                fill_color={'field': 'val', 'transform': color_mapper},
                fill_alpha=0.7, line_color="white", line_width=0.5)
 
-statePlot = figure(title="Coronavirus map", tools=TOOLS,
-                x_axis_location=None, y_axis_location=None,
-                tooltips=[('name','@name'),("value", "@val"), ("(Long, Lat)", "($x, $y)"), ('State', '@state')
-           ], height=300)
-color_mapper2 = LogColorMapper(palette=palette)
-statePlot.grid.grid_line_color = None
-# statePlot.x_range.start = -125
-# statePlot.x_range.end = -65
-# statePlot.y_range.start = 23
-# statePlot.y_range.end = 50
-statePlot.hover.point_policy = "follow_mouse"
-statePlot.patches('x', 'y', source=stateData,
-               fill_color={'field': 'val', 'transform': color_mapper2},
-               fill_alpha=0.7, line_color="white", line_width=0.5)
+def us_tap_handler(attr, old, new):
+    global state
+    # index = new[0]
+    # print(attr)
+    # print([x for x in list(locals().keys()) if x[0] != '_'])
+    if len(new) == 0:
+        print('US')
+        graphPlot.title.text = 'US'
+        gData.data = dict(
+            date=countriesDF['US'].time.astype(str),
+            time=countriesDF['US'].time,
+            confirmed=countriesDF['US'].confirmed,
+            recovered=countriesDF['US'].recovered,
+            deaths=countriesDF['US'].deaths)
+    else:
+        state = stateBorders.columns[new[0]]
+        print(state)
+        updateState()
+    return
 
-layout = column(usPlot, row(graphPlot, statePlot))
+def state_tap(attr, old, new):
+    global state, stateCountyDF, statesDF, stateBorders
+    print(state)
+    stateCountyDF = state_regionalDF[state_regionalDF['state'] == state]
+    print(new)
+    if len(new) == 0:
+        print(state)
+        graphPlot.title.text = stateBorders[state]['name']
+        gData.data = dict(
+            date=statesDF[state].time.astype(str),
+            time=statesDF[state].time,
+            confirmed=statesDF[state].confirmed,
+            recovered=statesDF[state].recovered,
+            deaths=statesDF[state].deaths)
+    else:
+        stateCountyDF = state_regionalDF[state_regionalDF['state'] == state]
+        stateCountyBorders = countyBorders[countyBorders['state'] == state]
+        county = stateCountyBorders.county_name.iloc[new[0]]
+        print(stateBorders[state]['name'])
+        print(county)
+        gData.data = dict(
+            date=stateCountyDF[stateCountyDF['county_name'] == county].time.values[0].astype(str),
+            time=stateCountyDF[stateCountyDF['county_name'] == county].time.values[0],
+            confirmed=stateCountyDF[stateCountyDF['county_name'] == county].confirmed.values[0],
+            recovered=stateCountyDF[stateCountyDF['county_name'] == county].recovered.values[0],
+            deaths=stateCountyDF[stateCountyDF['county_name'] == county].deaths.values[0])
+        graphPlot.title.text = county
+
+        # state = stateBorders.columns[new[0]]
+        # print(state)
+        # statePlot.title.text = stateBorders[state]['name']
+        # updateState(state=state)
+    return
+
+usData.selected.on_change("indices", us_tap_handler)
+stateData.selected.on_change("indices", state_tap)
+
+
+layout = column(row(usPlot, DataSelectButtons), row(graphPlot, statePlot))
+# show(layout)
+
+#
 doc = curdoc()
 doc.title = "Coronavirus Forecaster"
-# doc.add_root(layout)
+doc.add_root(layout)
+# # Run this in the command line:
 
-show(layout)
+# bokeh serve --show --log-level=debug US_coronavirus_map.py
